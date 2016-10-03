3b23832d285da59a4f075a65e025d0c9dc059414
positoy <positoy@gmail.com>
2016-10-03 17:56:17 +0900

    DP understanding applied to andy 'coin' code

--- Algorithm/week4/coins/Andy.cpp
+++ Algorithm/week4/coins/Andy.cpp
@@ -1,74 +1,71 @@
 #include <iostream>
-#include <cstdio>
 #include <memory.h>
-
 using namespace std;
 
-int T,N,M;
-int C[20];
+int T,N,COINS[20],M;
+int finished[20], d[20][10001];
 
-// user
-int d[20][10000];
+// 0~c 코인을 고려하여 M 금액을 만들 수 있는 경우의 수를 알고 있을 때,
+// 0~c 코인을 고려하여 M 금액을 만들 수 있는 경우의 수
+// 불가능하면 return -1
+void process(int c)
+{
+  int coin = COINS[c];
+
+  // 기저상태처리
+  if ( c>=N || c<0 ) return;
+  if ( c==0 )
+  {
+    for (int x=1; x*coin <= M; x++) d[0][coin*x] = 1;
+    finished[0] = 1;
+    return;
+  }
+
+  // 재귀
+  if ( finished[c-1]==-1 ) process(c-1);
+
+  // process
+  for (int x=0; x<=M; x++) d[c][x] = d[c-1][x];
+
+  for (int cost=1; cost<=M; cost++)
+  {
+    if ( cost<coin )
+    {
+      d[c][cost] = d[c-1][cost];
+      continue;
+    }
+    else
+    {
+      for (int x=1; x*coin<=cost; x++)
+      {
+        if ( cost==x*coin )
+          d[c][cost]++;
+        else if ( d[c-1][cost-x*coin] )
+          d[c][cost] += d[c-1][cost-x*coin];
+        else
+         continue;
+      }
+    }
+  }
+
+  finished[c] = 1;
+  return;
+}
 
 int main()
 {
-	cin >> T;
+  cin >> T;
 
-	for (int test_case=0; test_case<T; test_case++)
-	{
-		// input and initiation
-		cin >> N;
-		for (int i=0; i<N; i++)	cin >> C[i];
-		cin >> M;
+  for (int test_case=0; test_case<T; test_case++)
+  {
+    cin >> N;
+    for (int i=0; i<N; i++) cin >> COINS[i];
+    cin >> M;
 
-		// initiation
-		memset(d, 0x00, sizeof(d));
-		int coin = C[0];
-		for (int x = coin; x<=M; x+=coin) d[0][x] = 1;
+    memset(finished, -1, sizeof(finished));
+    memset(d, 0, sizeof(d));
+    process(N-1);
 
-		// process
-		for (int step = 1; step<N; step++)
-		{
-			// copy previous step
-			for (int amount=1; amount<=M; amount++)
-				d[step][amount] = d[step-1][amount];
-
-			// lets consider the new coin
-			coin = C[step];
-			for (int amount=1; amount <=M; amount++)
-			{
-				// how many coins can fit in amount?
-				for (int count=1; count*coin<=amount; count++)
-				{
-					int diff = amount - count*coin;
-					
-					if (diff == 0)
-					{
-						d[step][amount]++;
-					}
-					else
-					{
-						if (d[step-1][diff] != 0)
-							d[step][amount] += d[step-1][diff];
-
-					}
-				}
-			
-			}
-			#ifdef DEBUG
-			for (int i=1; i<=M; i++)
-			{
-				cout << d[step][i] << " ";
-			}	
-			cout << endl;
-			#endif
-
-		}	
-		cout << d[N-1][M] << endl;
-	}
+    cout << d[N-1][M] << endl;
+  }
 }
-
-		
-
-		
-
